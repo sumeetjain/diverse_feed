a22088f6ba9f8a89754beb266334dfa6e4b8f97a
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-22 17:04:15 -0600

    Add some homepage feature spec stubs

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "Homepage", type: :feature do
+  pending "shows a random person's diversity report summary"
+  pending "links to current user's account if logged in"
+  pending "links to begin authentication flow if not logged in"
+end
