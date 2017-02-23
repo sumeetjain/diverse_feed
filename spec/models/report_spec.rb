dcf22aa30433e087f9263cbd24a90ccf8d21571f
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-22 20:03:02 -0600

    Clean up and add some tests

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Report, type: :model do
-  pending "cannot be generated if report subject follows fewer than 50 accounts total"
-  pending "cannot be generated if report subject follows fewer than 10 accounts whose data is in our system"
+# RSpec.describe Report, type: :model do
+#   pending "cannot be generated if report subject follows fewer than 50 accounts total"
+#   pending "cannot be generated if report subject follows fewer than 10 accounts whose data is in our system"
 
-  pending "has total count of whom subject follows"
-  pending "has list of IDs for those whom subject follows"
-  pending "has a count of how many accounts comprise report data"
+#   pending "has total count of whom subject follows"
+#   pending "has list of IDs for those whom subject follows"
+#   pending "has a count of how many accounts comprise report data"
   
-  pending "has ethnicity breakdown"
-  pending "has gender breakdown"
-  pending "has sexual orientation breakdown"
-  pending "has political orientation breakdown"
-  pending "has age breakdown"
-  pending "has income breakdown"
-end
+#   pending "has ethnicity breakdown"
+#   pending "has gender breakdown"
+#   pending "has sexual orientation breakdown"
+#   pending "has political orientation breakdown"
+#   pending "has age breakdown"
+#   pending "has income breakdown"
+# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#find_or_create_from_twitter' do
+    it "finds an existing user" do
+      User.create(twitter_id: "abc")
+
+      auth_hash = {
+        uid: "abc"
+      }
+
+      user = User.find_or_create_from_twitter(auth_hash)
+
+      expect(user).to_not be_nil
+    end
+
+    it "creates user that doesn't exist yet" do
+      User.delete_all
+      
+      auth_hash = {
+        uid: "abc",
+        credentials: {
+          token: "foo",
+          secret: "bar"
+        }
+      }
+
+      user = User.find_or_create_from_twitter(auth_hash)
+
+      expect(user).to_not be_nil
+    end
+  end
 end
