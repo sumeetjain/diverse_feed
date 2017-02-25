29a6516cbe23450384bc4fc1b4b80a1abd094326
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-25 14:30:03 -0600

    Refactor: Rename var and reduce lines

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe TwitterService, type: :service do
   describe '#subject_friend_ids' do
     it "gets the IDs of the accounts that someone follows" do
-      asker = double
-      allow(asker).to receive(:twitter_key)
-      allow(asker).to receive(:twitter_secret)
+      asker = double(:asker, twitter_key: "key", twitter_secret: "secret")
 
-      service = TwitterService.new(asker: asker, client: FakeTwitter.new)
+      twitter = TwitterService.new(asker: asker, client: FakeTwitter.new)
 
-      expect(service.subject_friend_ids("hul")).to include(1)
+      expect(twitter.subject_friend_ids("hul")).to include(1)
     end
   end
 end
