289d168560c326612f90e8ed5c710cc4cd76d04a
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-25 15:33:23 -0600

    Added another test case, now failing

--- spec/models/demographic_mapper_spec.rb
+++ spec/models/demographic_mapper_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DemographicMapper, type: :model do
-  context 'for User #1' do
-    it 'has race breakdown' do
+  describe '#race' do
+    it 'maps one race per friend' do
       # User IDs and their corresponding race info.
       friends_info = {
         1 => ["White"],
@@ -17,6 +17,27 @@ RSpec.describe DemographicMapper, type: :model do
         "White"  => 50.0,
         "Black"  => 25.0,
         "Indian" => 25.0
+      })
+    end
+
+    it 'maps multiple races per friend' do
+      # User IDs and their corresponding race info.
+      friends_info = {
+        1 => ["White", "Mexican"],
+        2 => ["White", "Indian", "Black", "Chinese"],
+        3 => ["Black"],
+        4 => ["Indian", "Mexican", "Latino"]
+      }
+
+      mapper = DemographicMapper.new(friends_info)
+
+      expect(mapper.race).to include({
+        "White"  => 20.0,
+        "Mexican" => 20.0,
+        "Indian" => 20.0,
+        "Black"  => 20.0,
+        "Chinese" => 10.0,
+        "Latino" => 10.0
       })
     end
   end
