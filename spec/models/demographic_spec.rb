bcf0cd9883adbb1164c8b6f45f048865d745cb67
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-25 16:46:08 -0600

    Demographic Info

--- spec/models/demographic_spec.rb
+++ spec/models/demographic_spec.rb
@@ -13,5 +13,26 @@
 require 'rails_helper'
 
 RSpec.describe Demographic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.info_for' do
+    it "gets demographic data for the subject's friends" do
+      info = Demographic.info_for(1)
+
+      friends_data = {
+        race: {
+          1 => ["White"],
+          2 => ["White"],
+          3 => ["Black"],
+          4 => ["Indian"]
+        },
+        income: {
+          1 => 20000,
+          2 => 45000,
+          3 => 60000,
+          4 => 65000
+        }
+      }
+
+      expect(info).to include(friends_data)
+    end
+  end
 end
