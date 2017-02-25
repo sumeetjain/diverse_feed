c4801da28046adb586b346905d06db21e6458c34
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-25 15:17:50 -0600

    Test passes.

--- app/models/demographic_mapper.rb
+++ app/models/demographic_mapper.rb
@@ -1,2 +1,25 @@
 class DemographicMapper
+  attr_reader :friends_info
+
+  def initialize(friends_info)
+    @friends_info = friends_info
+  end
+
+  def race
+    @race ||= map_race
+  end
+
+  private
+
+  def map_race
+    @race = Hash.new(0)
+
+    share = 100.0 / @friends_info.length
+
+    @friends_info.each do |user_id, values|
+      values.each { |value| @race[value] += share }
+    end
+
+    @race
+  end
 end
--- spec/models/demographic_mapper_spec.rb
+++ spec/models/demographic_mapper_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe DemographicMapper, type: :model do
   context 'for User #1' do
     it 'has race breakdown' do
-      mapper = DemographicMapper.new
+      # User IDs and their corresponding race info.
+      friends_info = {
+        1 => ["White"],
+        2 => ["White"],
+        3 => ["Black"],
+        4 => ["Indian"]
+      }
+
+      mapper = DemographicMapper.new(friends_info)
 
       expect(mapper.race).to include({
-        white: 50.0,
-        black: 25.0,
-        indian: 25.0
+        "White"  => 50.0,
+        "Black"  => 25.0,
+        "Indian" => 25.0
       })
     end
   end
