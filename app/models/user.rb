8e863b9f0db448b35936449f5adb5433ac87d634
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-25 14:13:24 -0600

    Comment User model a bit. Also, users have many reports.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,27 @@
+# To submit their own demographic information, a person must be a User. That
+# means they have authenticated with Twitter, and we have captured their ID,
+# access token, and access secret. There is no other personally identifying
+# information about people in our system (e.g. SSN, name, email, etc).
+
 class User < ActiveRecord::Base
+  has_many :reports
+
+  # Initialize a User from the OAuth flow.
+  # 
+  # auth_hash - Hash containing the Twitter OAuth response.
+  # 
+  # Returns a User.
   def self.find_or_create_from_twitter(auth_hash)
     find_by_twitter_id(auth_hash[:uid]) || create_from_twitter(auth_hash)
   end
 
   private
 
+  # Create a new user from Twitter's OAuth flow.
+  # 
+  # auth_hash - Hash containing the Twitter OAuth response.
+  # 
+  # Returns a User.
   def self.create_from_twitter(auth_hash)
     user = self.new(
       twitter_id: auth_hash[:uid],
