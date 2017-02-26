92da83f4bb43db4d9e4b8c508fe3f82514dd7615
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-26 09:47:29 -0600

    Deleted some code left over from debugging

--- app/models/demographic_collector.rb
+++ app/models/demographic_collector.rb
@@ -6,16 +6,7 @@ class DemographicCollector
   def info
     sql = "SELECT * FROM crosstab('SELECT id, key, value FROM demographics WHERE user_id IN (#{@ids.join(",")}) ORDER BY 1', $$VALUES (1), (2)$$) AS (id int, race varchar, income varchar);"
     info = ActiveRecord::Base.connection.select_all(sql).entries
-
-    [{"id"=>"33", "race"=>"White", "income"=>nil},
-     {"id"=>"34", "race"=>nil, "income"=>"20000"},
-     {"id"=>"35", "race"=>"White", "income"=>nil},
-     {"id"=>"36", "race"=>nil, "income"=>"45000"},
-     {"id"=>"37", "race"=>"Black", "income"=>nil},
-     {"id"=>"38", "race"=>nil, "income"=>"60000"},
-     {"id"=>"39", "race"=>"Indian", "income"=>nil},
-     {"id"=>"40", "race"=>nil, "income"=>"65000"}]
-
+    
     friends_data = {
       "race"   => [],
       "income" => []
