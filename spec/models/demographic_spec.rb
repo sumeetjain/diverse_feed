0c8259ec85dfb9adfe11ec78ae73441699b4c150
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-26 01:38:37 -0600

    Test passes. Needs refactoring, and need to write up DB extention

--- spec/models/demographic_spec.rb
+++ spec/models/demographic_spec.rb
@@ -13,26 +13,4 @@
 require 'rails_helper'
 
 RSpec.describe Demographic, type: :model do
-  describe '.info_for' do
-    it "gets demographic data for the subject's friends" do
-      info = Demographic.info_for(1)
-
-      friends_data = {
-        race: {
-          1 => ["White"],
-          2 => ["White"],
-          3 => ["Black"],
-          4 => ["Indian"]
-        },
-        income: {
-          1 => 20000,
-          2 => 45000,
-          3 => 60000,
-          4 => 65000
-        }
-      }
-
-      expect(info).to include(friends_data)
-    end
-  end
 end
