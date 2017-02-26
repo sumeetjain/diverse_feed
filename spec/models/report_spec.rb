3107be35196f46759259627f8b64ca35be15030b
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-26 11:26:19 -0600

    Added some Report specs, failing tests

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -16,6 +16,41 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
+  describe '#generate' do
+    before :example do
+      @report = Report.new(subject: "hul", twitter_client: FakeTwitter.new)
+      @report.generate
+    end
+
+    it "sets twitter_id" do
+      expect(@report.twitter_id).to eq(1)
+    end
+
+    it "sets friends_count" do
+      expect(@report.friends_count).to eq(5)
+    end
+
+    it "sets friends_in_report_count" do
+      expect(@report.friends_count).to eq(4)
+    end
+
+    it "sets demographics, race" do
+      expect(@report.demographics[:race]).to include({
+        "White"  => 50.0,
+        "Black"  => 25.0,
+        "Indian" => 25.0
+      })
+    end
+
+    it "sets demographics, income" do
+      expect(@report.demographics[:income]).to include({
+        20000 => 25.0,
+        45000 => 25.0,
+        60000 => 25.0,
+        65000 => 25.0
+      })
+    end
+  end
 
   pending "cannot be generated if report subject follows fewer than 50 accounts total"
   pending "cannot be generated if report subject follows fewer than 10 accounts whose data is in our system"
