63feccaaf7f0599fcd553b15a5df67e758d6ea13
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-26 11:08:04 -0600

    Remove useless pending tests

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -20,11 +20,4 @@ RSpec.describe Report, type: :model do
   pending "cannot be generated if report subject follows fewer than 10 accounts whose data is in our system"
 
   pending "has a count of how many accounts comprise report data"
-  
-  pending "has ethnicity breakdown"
-  pending "has gender breakdown"
-  pending "has sexual orientation breakdown"
-  pending "has political orientation breakdown"
-  pending "has age breakdown"
-  pending "has income breakdown"
 end
