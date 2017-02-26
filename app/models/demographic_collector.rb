7e6cebbade327f9aaaa49e24637a07c151fbd2b9
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-26 09:49:40 -0600

    Beginning refactoring

--- app/models/demographic_collector.rb
+++ app/models/demographic_collector.rb
@@ -1,22 +1,31 @@
 class DemographicCollector
   def initialize(ids)
     @ids = ids
+
+    initialize_info_hash
   end
 
   def info
     sql = "SELECT * FROM crosstab('SELECT id, key, value FROM demographics WHERE user_id IN (#{@ids.join(",")}) ORDER BY 1', $$VALUES (1), (2)$$) AS (id int, race varchar, income varchar);"
     result = ActiveRecord::Base.connection.select_all(sql).entries
 
-    friends_data = {
+    
+
+    result.each do |row|
+      (@friends_data["race"]   << row["race"]) if row["race"]
+      (@friends_data["income"] << row["income"].to_i) if row["income"]
+    end
+
+    @friends_data
+  end
+
+  private
+
+  def initialize_info_hash
+    @friends_data = {
       "race"   => [],
       "income" => []
     }
-
-    result.each do |row|
-      (friends_data["race"]   << row["race"]) if row["race"]
-      (friends_data["income"] << row["income"].to_i) if row["income"]
-    end
-
-    friends_data
   end
+
 end
