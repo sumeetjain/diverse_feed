783a35937039cdd59b36d5fc6417c477b47bd376
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-08 20:03:27 -0600

    rails g controller homepage show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'homepage/show'
+
   resource :profile, only: [:show, :update]
   resources :reports, only: [:new, :create, :show]
 
   delete "/logout", to: 'sessions#destroy'
   get '/auth/:provider/callback', to: 'sessions#create'
+
+  root to: 'homepage#show'
 end
