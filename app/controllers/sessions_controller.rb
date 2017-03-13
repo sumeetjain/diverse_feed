e08f5a02a68d6a35f6953353cebf52d0b785e61a
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-13 14:25:32 -0500

    Fix up login/logout redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_twitter(auth_hash)
     session[:user_id] = user.id
+    redirect_to :root, notice: "Welcome!"
   end
 
   def destroy
     session[:user_id] = nil
+    redirect_to :root, notice: "You have been logged out."
   end
 
   private
