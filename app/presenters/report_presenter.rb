5b66f0dddb9221db0d05a81cff14c876393b8e0e
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-16 12:43:38 -0700

    ReportPresenter Docs

--- app/presenters/report_presenter.rb
+++ app/presenters/report_presenter.rb
@@ -8,24 +8,31 @@ class ReportPresenter
     @report = report
   end
 
+  # Returns an Array of all of this report's graphs.
   def graphs
     labels.map { |key| GraphPresenter.new(key, @report.demographics[key]) }
   end
 
+  # Returns a String of this report's subject (e.g. "@sumeetjain").
   def subject
     "@#{report.subject}"
   end
 
+  # Returns a String containing how many accounts the subject follows.
   def friends_count
     "Follows #{report.friends_count} accounts"
   end
 
+  # Returns a String indicating how many of the subject's friends we have
+  # demographic data for.
   def friends_in_report_percentage
     "Report based upon #{report.friends_in_report_percentage}% of accounts followed"
   end
 
   private
 
+  # Returns an Array containing only those demographic keys (e.g. 'race',
+  # 'gender') for which there is actually information to show.
   def labels
     @report.demographics.map { |key, values| key if !values.blank? }.compact
   end
