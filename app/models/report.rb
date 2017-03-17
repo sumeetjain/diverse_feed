f4e010184b9082bf799163f338aa730411efdbc0
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-17 14:54:33 -0700

    Fix error message

--- app/models/report.rb
+++ app/models/report.rb
@@ -29,7 +29,7 @@ class Report < ActiveRecord::Base
       # object = person object being validated
       # data = { model: "Person", attribute: "Username", value: <username> }
       message: -> (object, data) do
-        "Only #{data[:value]} of the accounts this person follows have provided demographic information to us. Unfortunately, that's not enough for us to build a meaningful report. Please try again some time in the future, when hopefully more of them will have participated in this project."
+        "Only #{data[:value].to_i} of the accounts this person follows have provided demographic information to us. Unfortunately, that's not enough for us to build a meaningful report. Please try again some time in the future, when hopefully more of them will have participated in this project."
       end
     },
     if: :fetched_friends
